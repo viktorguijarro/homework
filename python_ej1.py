1e4a57b435a11f514f950ce948aa69c90dd17357
Viktor Manuel Guijarro-Heeb <viktor@vikdell>
2020-10-18 17:09:30 +0200

    first commit

---
+++ python_ej1.py
@@ -0,0 +1,12 @@
+a=float(input("Entra un numero: "))
+b=float(input("Entra un numero: "))
+c=float(input("Entra un numero: "))
+m=a+b+c
+print("El valor es: ", m)
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
